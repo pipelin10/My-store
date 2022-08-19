1aa800ac8e510c1f31675cf11608735df50a85b6
pipelin10 <carlosfemontoya@gmail.com>
2022-08-19 13:03:16 -0500

    deploy

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const routerApi = require('./routes')
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorHandler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 const whitelist = ['http://localhost:8080', 'https://myapp.co']
 const options = {
   origin: (origin, callback) => {
-    if(whitelist.includes(origin)){
+    if(whitelist.includes(origin) || !origin){
       callback(null,true)
     }else{
       callback(new Error('no allowed'), false)
